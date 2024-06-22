73c6b26dcd604514eb79c1cc0ab3982b3f78cab7
sailesh <vermasailesh271@gmail.com>
2024-06-22 16:55:41 +0530

    added social links

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,6 +2,7 @@ import "./NavigationBar.css";
 import github from "../../assets/icons/github.svg";
 import linkedin from "../../assets/icons/linkedin.svg";
 import playstore from "../../assets/icons/playstore.svg";
+import { UserSocialUrl } from "./constants";
 
 function NavigationBar() {
   return (
@@ -9,33 +10,57 @@ function NavigationBar() {
       <div className="row-container">
         <h1>Logo</h1>
         <div className="Nav-links">
-          <div className="nav-link-item">Home</div>
-          <div className="nav-link-item">Skills</div>
-          <div className="nav-link-item">Projects</div>
+          <a href="/home" className="nav-link-item">
+            Home
+          </a>
+          <a href="/home" className="nav-link-item">
+            Skills
+          </a>
+          <a href="/home" className="nav-link-item">
+            Projects
+          </a>
         </div>
       </div>
 
       <div>
         <div className="Nav-social-links">
-          <div className="Nav-social-icon">
-            <a href="">
-              <img height={20} width={20} alt="linkedIn" src={linkedin} />
-            </a>
-          </div>
-          <div className="Nav-social-icon">
-            <a href="">
-              <img height={20} width={20} alt="github" src={github} />
-            </a>
-          </div>
-          <div className="Nav-social-icon">
-            <a href="">
-              <img height={20} width={20} alt="playstore" src={playstore} />
-            </a>
-          </div>
+          <a
+            className="Nav-social-icon"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={UserSocialUrl.LINKEDIN}
+          >
+            <img height={20} width={20} alt="linkedIn" src={linkedin} />
+          </a>
+
+          <a
+            className="Nav-social-icon"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={UserSocialUrl.GITHUB}
+          >
+            <img height={20} width={20} alt="github" src={github} />
+          </a>
+
+          <a
+            className="Nav-social-icon"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={UserSocialUrl.PLAYSTORE}
+          >
+            <img height={20} width={20} alt="playstore" src={playstore} />
+          </a>
         </div>
       </div>
 
-      <div className="nav-button">Let's Connect</div>
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        className="nav-button"
+        href={UserSocialUrl.GMAIL}
+      >
+        Let's Connect
+      </a>
     </div>
   );
 }
