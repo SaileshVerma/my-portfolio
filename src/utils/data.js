44f53fa40e13237294ef06ef0ed88ecde65bb62b
sailesh <vermasailesh271@gmail.com>
2024-06-23 17:44:29 +0530

    added data & req packages

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import awsIcon from "../assets/icons/aws.svg";
+import cIcon from "../assets/icons/c.svg";
+import cssIcon from "../assets/icons/css.svg";
+import dartIcon from "../assets/icons/dart.svg";
+import expressJsIcon from "../assets/icons/expressjs.svg";
+import fireBaseIcon from "../assets/icons/firebase.svg";
+import flutterIcon from "../assets/icons/flutter.svg";
+import graphqlIcon from "../assets/icons/graphql.svg";
+import javascriptIcon from "../assets/icons/javascript.svg";
+import mySQLIcon from "../assets/icons/mysql.svg";
+import nestJSIcon from "../assets/icons/nestjs.svg";
+import nextJSIcon from "../assets/icons/nextjs.svg";
+import nodeJSIcon from "../assets/icons/nodejs.svg";
+import postgresIcon from "../assets/icons/postgres.svg";
+import tailWindCssIcon from "../assets/icons/tailwind.svg";
+import typeScriptIcon from "../assets/icons/typescript.svg";
+
+export const CarouselData = [
+  { iconSource: awsIcon, title: "AWS" },
+  { iconSource: cIcon, title: "C" },
+  { iconSource: cssIcon, title: "CSS" },
+  { iconSource: dartIcon, title: "DART" },
+  { iconSource: expressJsIcon, title: "ExpressJS" },
+  { iconSource: fireBaseIcon, title: "Firebase" },
+  { iconSource: flutterIcon, title: "Flutter" },
+  { iconSource: graphqlIcon, title: "GraphQL" },
+  { iconSource: javascriptIcon, title: "JavaScript" },
+  { iconSource: mySQLIcon, title: "MySQL" },
+  { iconSource: nestJSIcon, title: "NestJS" },
+  { iconSource: nextJSIcon, title: "NextJS" },
+  { iconSource: nodeJSIcon, title: "NodeJS" },
+  { iconSource: postgresIcon, title: "Postgres" },
+  { iconSource: tailWindCssIcon, title: "TailWindCSS" },
+  { iconSource: typeScriptIcon, title: "TypeScript" },
+];
