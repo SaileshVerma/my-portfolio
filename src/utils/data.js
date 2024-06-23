9f5c4e2257fca7aac02c20a5e4532b7d57a74c6f
sailesh <vermasailesh271@gmail.com>
2024-06-24 00:33:48 +0530

    added req package and data

--- src/utils/data.js
+++ src/utils/data.js
@@ -6,6 +6,7 @@ import expressJsIcon from "../assets/icons/expressjs.svg";
 import fireBaseIcon from "../assets/icons/firebase.svg";
 import flutterIcon from "../assets/icons/flutter.svg";
 import graphqlIcon from "../assets/icons/graphql.svg";
+import htmlIcon from "../assets/icons/html.svg";
 import javascriptIcon from "../assets/icons/javascript.svg";
 import mySQLIcon from "../assets/icons/mysql.svg";
 import nestJSIcon from "../assets/icons/nestjs.svg";
@@ -16,20 +17,62 @@ import tailWindCssIcon from "../assets/icons/tailwind.svg";
 import typeScriptIcon from "../assets/icons/typescript.svg";
 
 export const CarouselData = [
-  { iconSource: awsIcon, title: "AWS" },
-  { iconSource: cIcon, title: "C" },
+  { iconSource: cIcon, title: "C++" },
   { iconSource: cssIcon, title: "CSS" },
-  { iconSource: dartIcon, title: "DART" },
+  { iconSource: htmlIcon, title: "HTML" },
+  { iconSource: tailWindCssIcon, title: "TailWindCSS" },
+  { iconSource: flutterIcon, title: "Flutter" },
+  { iconSource: dartIcon, title: "Dart" },
   { iconSource: expressJsIcon, title: "ExpressJS" },
   { iconSource: fireBaseIcon, title: "Firebase" },
-  { iconSource: flutterIcon, title: "Flutter" },
   { iconSource: graphqlIcon, title: "GraphQL" },
   { iconSource: javascriptIcon, title: "JavaScript" },
-  { iconSource: mySQLIcon, title: "MySQL" },
+  { iconSource: typeScriptIcon, title: "TypeScript" },
   { iconSource: nestJSIcon, title: "NestJS" },
   { iconSource: nextJSIcon, title: "NextJS" },
   { iconSource: nodeJSIcon, title: "NodeJS" },
+  { iconSource: mySQLIcon, title: "MySQL" },
   { iconSource: postgresIcon, title: "Postgres" },
-  { iconSource: tailWindCssIcon, title: "TailWindCSS" },
-  { iconSource: typeScriptIcon, title: "TypeScript" },
+  { iconSource: awsIcon, title: "AWS" },
+];
+
+export const projectData = [
+  {
+    title: "Wake N Walk",
+    desc: "Say goodbye to sluggish mornings and hello to a vibrant, goal-driven start with Wake N Walk. Set your alarm and commit to your goals—the alarm only turns off once you've hit your target steps. ",
+    projectLink:
+      "https://play.google.com/store/apps/details?id=com.osudevs.wake_n_walk",
+    techUsed: "Flutter, Dart, Sqflite, Pedometer",
+  },
+  {
+    title: "Oldus",
+    desc: "A flutter app for the old age homes where user can see the details(volunteer works, donation, location, etc) of the registered old age homes and various activities associated with it",
+    projectLink: "https://github.com/SaileshVerma/Oldus",
+    techUsed: "Flutter, Dart, Firebase",
+  },
+  {
+    title: "React:ToDo",
+    desc: "A to do app made using react and vite js. It offers a simple yet powerful interface for adding, organizing, and completing tasks, making it an essential tool for personal productivity and project management",
+    projectLink: "https://github.com/SaileshVerma/React-To-Do",
+    techUsed: "ReactJS, Javascript, ViteJS, TailwindCSS",
+  },
+  {
+    title: "Patient Dashboard",
+    desc: "A Patient Dashboard is an integrated, user-friendly interface designed to provide comprehensive insights and details about patients in a healthcare setting. This dashboard serves as a central hub for healthcare providers, offering quick access to critical patient information, analytics, and doctor-patient interactions.",
+    projectLink:
+      "https://patient-dashboard-1i4f-ommc26muv-saileshvermas-projects.vercel.app/",
+    techUsed: "NextJS, TailwindCSS",
+  },
+  {
+    title: "NotesGallery",
+    desc: "NotesGallery is an innovative platform designed to streamline the management and distribution of notes in PDF format. It is tailored to cater to the needs of both administrators and users, providing a clear delineation of roles and access privileges to ensure a secure and organized repository of notes. ",
+    projectLink: "https://github.com/SaileshVerma/NotesGallery",
+    techUsed: "Flutter, Dart, ExpressJS",
+  },
+  {
+    title: "Spam-Ham Detection",
+    desc: "A spam message detector using pythonSpam Ham Detection is a sophisticated application leveraging Python and Machine Learning (ML) techniques to classify incoming messages as either spam (unsolicited or unwanted messages) or ham (genuine messages)",
+    projectLink: "https://github.com/SaileshVerma/SpamHamDetection",
+    techUsed: "Python, NLP",
+  },
 ];
