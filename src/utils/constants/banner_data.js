fa7913372af3ad3ccf76d031482b2943fa145a4a
sailesh <vermasailesh271@gmail.com>
2024-06-30 19:51:44 +0530

    added constants value for user data

---
+++ src/utils/constants/banner_data.js
@@ -0,0 +1,5 @@
+export const UserProfileData = {
+  NAME: "Sailesh Verma",
+  ROLE: ["Web  Developer", "Software Developer", "Mobile App Developer"],
+  BIO: "Enthusiastic and innovative Software Developer with a passion for crafting seamless and user-friendly mobile applications and robust backend systems. I am passionate about creating innovative solutions that make a meaningful impact.",
+};
---
+++ src/utils/constants/nav_bar_data.js
@@ -0,0 +1,11 @@
+export const UserSocialUrl = {
+  LINKEDIN_URL: "https://www.linkedin.com/in/sailesh-verma-2509031a4/",
+  TWITTER_URL: "https://x.com/vermasailesh271",
+  MEDIUM_URL: "https://medium.com/@vermasailesh271",
+  LEETCODE_URL: "https://leetcode.com/u/vermasailesh271/",
+  PLAYSTORE_URL:
+    "https://play.google.com/store/apps/dev?id=7370046320618077246",
+  GITHUB_URL: "https://play.google.com/store/apps/dev?id=7370046320618077246",
+  LETS_CONNECT_GMAIL:
+    "https://mail.google.com/mail/?view=cm&fs=1&to=vermasailesh271@gmail.com&su=Hello&body=Hi%20Sailesh,%0D%0A%0D%0AI%20wanted%20to%20reach%20out%20to%20you%20regarding...",
+};
